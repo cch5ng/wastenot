1432169a4c751afe6f45dd0426dd5fe4a5fe0fd2
Carol Chung <cch5ng@gmail.com>
2019-09-28 22:21:22 -0700

    refactor to use Lists and helper

--- client/src/ListTemplates/ListTemplates.js
+++ client/src/ListTemplates/ListTemplates.js
@@ -4,8 +4,8 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 //import useListTemplates from '../utils/hooks/useListTemplates';
 
 //import {FaPlusSquareO} from 'react-icons/fa';
-
-//import Lists from '../Lists/components/Lists'
+import Lists from '../Lists/components/Lists';
+import { objToArray } from '../utils/utils';
 //import '../App.css';
 
 const ListTemplates = (props) => {
@@ -14,23 +14,15 @@ const ListTemplates = (props) => {
 
   console.log('listTemplates', listTemplates);
 
-  function objToArray(obj) {
-    let resultAr = []
-    Object.keys(obj).forEach(ok => {
-      resultAr.push(obj[ok])
-    })
-
-    return resultAr
-  }
-
   let listTemplatesAr = []
   if (props.listTemplates) {
-    listTemplatesAr = objToArray(props.listTemplates)
+    listTemplatesAr = objToArray(listTemplates)
   }
 
   return (
     <div className="main">
       <Link to="/settings/listTemplatesNew"><IoIosAddCircleOutline className="list-item-icon-lg" /> New Template List</Link>
+      <Lists lists={listTemplatesAr} type="template" />
     </div>
   )
 
