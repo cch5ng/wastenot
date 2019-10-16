7954f37862500dd5fc3bf94b06f62f1ccb08c92a
Carol Chung <cch5ng@gmail.com>
2019-10-16 14:23:38 -0700

    stub out logic for create new list

--- server/app/list/table.js
+++ server/app/list/table.js
@@ -1,19 +1,48 @@
 const pool = require('../../databasePool');
+const uuidv4 = require('uuid/v4');
 
 class ListTable {
 
-	// static storeList({ list }) {
-	// 	return new Promise((resolve, reject) => {
-	// 		pool.query(
-	// 			`SELECT * from `,
-	// 			[list],
-	// 			(error, response) => {
-	// 				if (error) return reject(error);
-	// 				resolve(response.rows);
-	// 			}
-	// 		)
-	// 	})
-	// }
+	static storeList({ list }) {
+		const { name, type, listItems } = list;
+		return new Promise((resolve, reject) => {
+			pool.query(
+				`INSERT INTO list (name, type, guid) VALUES ($1, $2, $3)`,
+				[name, type, uuidv4()],
+				(error, response) => {
+					if (error) return reject(error);
+						if (response.rows.length && response.rows[0].guid) {
+							const list_guid = response.rows[0].guid;
+							const listId = response.rows[0].id;
+
+							Promise.all(
+								listItems.map(({ name }) => {
+									let guid = uuidv4();
+									return ListItemTable.storeListItem({ name, listId, guid })
+								})
+							)
+								.then(() => resolve({ list_guid }))
+								.catch(err => reject(err))
+						}
+				}
+			)
+		})
+	}
+
+	static storeListItem({ name, listId, guid }) {
+		return new Promise((resolve, reject) => {
+			pool.query(
+				`INSERT INTO list_item (name, list_id, guid) VALUES ($1, $2, $3)`,
+				[name, listId, guid],
+				(error, response) => {
+					if (error) return reject(error);
+					if (response.rows.length) {
+						resolve();
+					}
+				}
+			)
+		})
+	}
 
 	static getListsByType({ listType }) {
 		return new Promise((resolve, reject) => {
@@ -30,11 +59,11 @@ class ListTable {
 	}
 
 	//probably want to get everything like name plus the list items
-	static getListDetailById({ listId }) {
+	static getListDetailById({ listGuid }) {
 		return new Promise((resolve, reject) => {
 			pool.query(
 				`SELECT * from list WHERE id = $1`,
-				[listId],
+				[listGuid],
 				(error, response) => {
 					if (error) return reject(error);
 					if (response.rows.length) {
