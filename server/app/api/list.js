5f6eaa4025603c22e8f050f3e794e5eb44b1d510
Carol Chung <cch5ng@gmail.com>
2019-10-16 13:42:37 -0700

    add init server routes for list

---
+++ server/app/api/list.js
@@ -0,0 +1,38 @@
+const { Router } = require('express');
+//const passport = require('passport');
+const ListTable = require('../list/table');
+
+const router = Router();
+
+router.get('/shoppingLists', (req, res, next) => {
+	ListTable.getListsByType({ listType: 'shopping' })
+		.then(lists => res.json(lists))
+		.catch(err => next(err));
+		//console.error('error', err));
+});
+
+router.get('/templateLists', (req, res, next) => {
+	ListTable.getListsByType({ listType: 'template' })
+		.then(lists => res.json(lists))
+		.catch(err => next(err));
+		//console.error('error', err));
+});
+
+router.get('/listDetail', (req, res, next) => {
+	ListTable.getListDetailById(req.body)
+		.then(list => res.json(list))
+		.catch(err => next(err));
+		//console.error('error', err));
+});
+
+
+
+// router.post('/random',
+// 	passport.authenticate('jwt', { session: false }),
+// 	(req, res, next) => {
+// 	QuestionTable.getRandomQuestionsByCategoryCounts(req.body)
+// 		.then(testLists => res.json(testLists))
+// 		.catch(err => console.error('error', err))
+// })
+
+module.exports = router;
