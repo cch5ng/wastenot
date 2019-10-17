4abfd510bfcb01efaf585e83af18ae40338a7965
Carol Chung <cch5ng@gmail.com>
2019-10-17 12:12:58 -0700

    change the foreign key for list_item table

--- server/bin/sql/list_item.sql
+++ server/bin/sql/list_item.sql
@@ -2,7 +2,7 @@ CREATE TABLE list_item(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(64),
 	sort_order INTEGER,
-	list_id INTEGER,
+	list_guid VARCHAR(64),
 	guid VARCHAR(64),
-	FOREIGN KEY (list_id) REFERENCES list(id)
+	FOREIGN KEY (list_guid) REFERENCES list(guid)
 );
