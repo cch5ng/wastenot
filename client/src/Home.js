3a498d39d567bb9a173872d02556b7dcc181df86
cch5ng <cch5ng@gmail.com>
2019-11-01 17:51:14 -0700

    add redux store

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,27 +1,45 @@
 // src/Home.js
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { withAuth } from '@okta/okta-react';
+import { setAuthenticated, setNotAuthenticated, setToken } from './actions/auth';
 
-export default withAuth(class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = { authenticated: null };
-    this.checkAuthentication = this.checkAuthentication.bind(this);
-    this.checkAuthentication();
+    //this.checkAuthentication = this.checkAuthentication.bind(this);
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
   }
 
-  async checkAuthentication() {
+    //change this to calling an action
+  async componentDidMount() {
     const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState({ authenticated });
+    if (this.props.auth && authenticated !== this.props.auth.isAuthenticated) {
+      if (authenticated) {
+        this.props.setAuthenticated();
+        const token = this.props.auth.getAccessToken();
+        this.props.setToken(token);
+      } else {
+        this.props.setNotAuthenticated();
+      }
     }
   }
 
-  componentDidUpdate() {
-    this.checkAuthentication();
+  async componentDidUpdate() {
+    // const authenticated = await this.props.auth.isAuthenticated();
+    // if (this.props.auth && authenticated !== this.props.auth.isAuthenticated) {
+    //   if (authenticated) {
+    //     this.props.setAuthenticated();
+    //     const token = this.props.auth.getAccessToken();
+    //     this.props.setToken(token);
+    //   } else {
+    //     this.props.setNotAuthenticated();
+    //   }
+    // }
   }
 
   async login() {
@@ -35,9 +53,37 @@ export default withAuth(class Home extends Component {
   }
 
   render() {
-    //if (this.state.authenticated === null) return null;
-    return this.state.authenticated ?
+    if (this.props.auth) {
+      console.log('auth', this.props.auth);
+    }
+
+    //change this to checking redux
+    return (this.props.auth && this.props.auth.isAuthenticated) ?
       <button onClick={this.logout}>Logout</button> :
       <button onClick={this.login}>Login</button>;
   }
-});
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+})
+
+const mapDispatchToProps = dispatch => ({
+  setAuthenticated: () => dispatch(setAuthenticated()),
+  setNotAuthenticated: () => dispatch(setNotAuthenticated()),
+  setToken: (token) => dispatch(setToken(token))
+})
+
+// const enhance = compose(
+//   // These are both single-argument HOCs
+//   withAuth,
+//   connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+//   )(Home)
+// )
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withAuth(Home));
