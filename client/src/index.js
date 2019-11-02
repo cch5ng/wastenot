2d1e7df1ce550c7aa557a1f58153bb6a3c4b0931
cch5ng <cch5ng@gmail.com>
2019-11-01 17:50:35 -0700

    update for thunk and redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, document.getElementById('root'));
