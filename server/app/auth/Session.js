2028cf2230f22292690b604e1b298dfcea36b3c8
cch5ng <cch5ng@gmail.com>
2019-11-09 14:25:55 -0800

    add methods to parse and verify session data; debug and separate sessionHash key from hash method

--- server/app/auth/Session.js
+++ server/app/auth/Session.js
@@ -1,10 +1,12 @@
-const uuid = require('./uuid');
+const uuidv4 = require('uuid/v4');
 const { hash } = require('./helper');
+
+const SEPARATOR = '|';
 
 class Session {
   constructor({ email }) {
     this.email = email;
-    this.id = uuid();
+    this.id = uuidv4();
   }
 
   toString() {
@@ -12,14 +14,37 @@ class Session {
     return Session.sessionString({ email, id });
   }
 
+  //convert string formatted session data to obj
+  static parse(sessionStr) {
+    const sessionAr = sessionStr.split(SEPARATOR);
+
+    return {
+      email: sessionAr[0],
+      id: sessionAr[1],
+      sessionHash: sessionAr[2]
+    };
+  }
+
+  static verify(sessionStr) {
+    const { email, id, sessionHash } = Session.parse(sessionStr);
+    const acData = Session.accountData({ email, id });
+
+    return hash(acData) === sessionHash;
+  }
+
   static accountData({ email, id }) {
-    return `${email}|${id}`;
+    return `${email}${SEPARATOR}${id}`;
   }
 
   static sessionString({ email, id }) {
     const accountData = Session.accountData({ email, id });
-    return `${accountData}|${hash(accountData)}`;
+    return `${accountData}${SEPARATOR}${hash(accountData)}`;
   }
 }
+
+const foo = new Session({ email: 'foo@f.com '});
+const fooStr = foo.toString();
+console.log('Session.parse(fooStr)', Session.parse(fooStr));
+console.log('Session.verify(fooStr)', Session.verify(fooStr));
 
 module.exports = Session;
