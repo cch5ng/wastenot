ac029a66674a040eb32f047902a4b076b2c6b970
cch5ng <cch5ng@gmail.com>
2019-11-09 23:14:34 -0800

    update user table to include sessionId

--- server/bin/sql/wastenot_user.sql
+++ server/bin/sql/wastenot_user.sql
@@ -1,8 +1,9 @@
 CREATE TABLE wastenot_user(
-	id SERIAL PRIMARY KEY,
-	"emailHash" VARCHAR(64) UNIQUE,
+	id             SERIAL PRIMARY KEY,
+	"emailHash"    VARCHAR(64) UNIQUE,
 	"passwordHash" VARCHAR(64),
-	name VARCHAR(64),
-	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-	guid VARCHAR(64) UNIQUE
+	name           VARCHAR(64),
+	created_at     TIMESTAMP NOT NULL DEFAULT NOW(),
+	guid           VARCHAR(64) UNIQUE,
+  "sessionId"    VARCHAR(36)
 );
