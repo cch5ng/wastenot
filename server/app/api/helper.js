0e1a4663806784cf5646a4a0ded3894bf8a744bb
cch5ng <cch5ng@gmail.com>
2019-11-11 17:22:23 -0800

    refactor setSession() to handle case where sessionId already exists in db; handle multiple devices sharing session

--- server/app/api/helper.js
+++ server/app/api/helper.js
@@ -2,26 +2,38 @@ const Session = require('../auth/Session');
 const AuthTable = require('../auth/table');
 const { hash } = require('../auth/helper');
 
-const setSession = ({ email, res }) => {
+const setSession = ({ email, res, sessionId }) => {
   return new Promise((resolve, reject) => {
-    const session = new Session({ email });
-    const sessionStr = session.toString();
+    const session, sessionStr;
 
-    AuthTable.updateSessionId({ 
-      sessionId: session.id,
-      emailHash: hash(email)
-    })
-      .then(() => {
-        res.cookie('sessionStr', sessionStr, {
-          expire: Date.now() + 3600000,
-          httpOnly: true
-          //secure: true
-        });
+    //case user already has sessionId saved in db
+    if (session) {
+      sessionStr = Session.sessionStr({ email, id: sessionId });
+      setSessionCookie({ sessionStr, res });
+      resolve({ message: 'session restored'});
+    } else {
+      //case no user sessionId found in db
+      session = new Session({ email });
+      sessionStr = session.toString();
 
-        resolve({ message: 'session created' });
+      AuthTable.updateSessionId({ 
+        sessionId: session.id,
+        emailHash: hash(email)
       })
-      .catch(err => reject(err))
+        .then(() => {
+          setSessionCookie({ sessionStr, res });
+          resolve({ message: 'session created' });
+        })
+        .catch(err => reject(err))
+    }
+  });
+}
 
+const setSessionCookie = ({ sessionStr, res }) => {
+  res.cookie('sessionStr', sessionStr, {
+    expire: Date.now() + 3600000,
+    httpOnly: true
+    //secure: true
   });
 }
 
