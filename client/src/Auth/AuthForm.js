12a62545ba1658ecf38ff3410ff27067107266ee
cch5ng <cch5ng@gmail.com>
2019-11-13 20:58:37 -0800

    stub out ev handlers for inputs and buttons

--- client/src/Auth/AuthForm.js
+++ client/src/Auth/AuthForm.js
@@ -7,18 +7,44 @@ class AuthForm extends Component {
     password: ''
   }
 
+  updateInput = ev => {
+    let name = ev.target.name;
+    let value = ev.target.value;
+    this.setState({[name]: value})
+  }
+
+  logInBtnClick = (ev) => {
+    ev.preventDefault();
+    console.log('state', this.state)
+  }
+
+  signInBtnClick = (ev) => {
+    ev.preventDefault();
+    console.log('state', this.state)
+  }
+
   render() {
     return (
       <div>
         <h1>AuthForm</h1>
         <form>
-          <input type="text" value={this.state.email} placeholder="email" />
+          <input type="text"
+            name="email"
+            value={this.state.email}
+            placeholder="email"
+            onChange={this.updateInput}
+          />
           <div />
-          <input type="password" value={this.state.password} placeholder="password" />
+          <input type="password"
+            name="password"
+            value={this.state.password}
+            placeholder="password"
+            onChange={this.updateInput}
+          />
           <div />
-          <button>Log In</button>
+          <button onClick={this.logInBtnClick}>Log In</button>
           <span> or </span>
-          <button>Sign Up</button>
+          <button onClick={this.signInBtnClick}>Sign Up</button>
         </form>
       </div>
     )
