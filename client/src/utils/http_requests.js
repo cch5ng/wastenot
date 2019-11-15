58fc0faf4d5c747fea28503a2da4fdcc9ed7ffb3
cch5ng <cch5ng@gmail.com>
2019-11-14 23:48:58 -0800

    stubbed out http requests for auth

--- client/src/utils/http_requests.js
+++ client/src/utils/http_requests.js
@@ -1,31 +1,30 @@
 const API_ROOT = process.env.API_ROOT;
 //URL MIDDLE
 const API_LIST_MIDDLE = '/list';
+const API_AUTH_MIDDLE = '/auth';
 //URL SUFFIXES
 const CREATE_LIST_SUFFIX = '/add';
 const GET_SHOPPING_LISTS_SUFFIX = '/shoppingLists';
 const GET_TEMPLATE_LISTS_SUFFIX = '/templateLists';
 const LIST_DETAIL_SUFFIX = '/listDetail';
+const AUTH_REGISTER_SUFFIX = '/register';
+const AUTH_LOGIN_SUFFIX = '/login';
+const AUTH_LOGOUT_SUFFIX = '/logout';
 
 const requests = {
-  get: (url, token) => {
+  get: (url) => {
     return new Promise((resolve, reject) => {
-      fetch(url, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      })
+      fetch(url)
         .then(resp => resolve(resp.json()))
         .catch(err => reject(err));
       //console.error('fetch error', err));
     });
   },
-  post: (url, token, body) => {
+  post: (url, body) => {
     return new Promise((resolve, reject) => {
       fetch(url, {
         method: 'POST',
         headers: {
-          'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(body),
@@ -37,17 +36,30 @@ const requests = {
 };
 
 const Lists = {
-  getAllShoppingLists: (token) => {
+  getAllShoppingLists: () => {
     return requests.get(`${API_ROOT}${API_LIST_MIDDLE}${GET_SHOPPING_LISTS_SUFFIX}`, token);
   },
-  getAllTemplateLists: (token) => {
+  getAllTemplateLists: () => {
     return requests.get(`${API_ROOT}${API_LIST_MIDDLE}${GET_TEMPLATE_LISTS_SUFFIX}`, token);
   },
-  postTemplateList: (token, list) => {
+  postTemplateList: (list) => {
     return requests.post(`${API_ROOT}${API_LIST_MIDDLE}${CREATE_LIST_SUFFIX}`, token, list)
   }
 };
 
+const Auth = {
+  postRegister: (email, password) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_REGISTER_SUFFIX}`, {email, password});
+  },
+  postLogin: (email, password) => {
+    return requests.post(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_LOGIN_SUFFIX}`, {email, password});
+  },
+  getLogout: () => {
+    return requests.get(`${API_ROOT}${API_AUTH_MIDDLE}${AUTH_LOGOUT_SUFFIX}`);
+  }
+}
+
 export default {
   Lists,
+  Auth,
 };
