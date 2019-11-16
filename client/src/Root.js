ee55d4824e15c51dd2642cfc37fff5f8a097a3ba
cch5ng <cch5ng@gmail.com>
2019-11-15 16:28:04 -0800

    refactor to show form when not logged in

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Home from './Home';
 import AuthForm from './Auth/AuthForm';
 
 class Root extends Component {
 
   render() {
+    let isLoggedIn = this.props.authenticate.isLoggedIn;
+    console.log('isLoggedIn', isLoggedIn)
     return (
-      false ? <Home /> : <AuthForm />
+      {isLoggedIn} === true ? <Home /> : <AuthForm />
     )
   }
-
 }
 
-export default Root;
+const mapStateToProps = (state) => ({ authenticate: state.authenticate });
+
+export default connect(
+  mapStateToProps,
+  null)(Root);
