0e7b638f99d9bd87f6e3b4c23a81c24b8e7ef48d
cch5ng <cch5ng@gmail.com>
2019-11-20 16:57:58 -0800

    src credit for checking sessionStor available

--- client/src/actions/authenticate.js
+++ client/src/actions/authenticate.js
@@ -96,6 +96,7 @@ export const logout = () => dispatch => {
 }
 
 //helper
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
 
 function storageAvailable(type) {
     var storage;
