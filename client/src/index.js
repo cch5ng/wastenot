f7211e0f26e3420e257438be6134707e00d83129
cch5ng <cch5ng@gmail.com>
2019-11-29 20:32:01 -0800

    cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,18 @@ import 'regenerator-runtime/runtime';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'
+import rootReducer from './reducers';
+import { isAuthenticated } from './actions/authenticate';
 import './index.css';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
+const store = createStore(rootReducer, 
+  composeEnhancers(
+    applyMiddleware(thunk)
 ));
+
+store.dispatch(isAuthenticated());
 
 ReactDOM.render(
   <Provider store={store} >
