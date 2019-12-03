23a7a5d881c4d2159f03cff2759cba37e8ba04ee
cch5ng <cch5ng@gmail.com>
2019-12-02 19:40:49 -0800

    update view to get list from BE

--- client/src/ListTemplates/ListTemplates.js
+++ client/src/ListTemplates/ListTemplates.js
@@ -8,41 +8,71 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import Lists from '../Lists/Lists';
 import { objToArray } from '../utils/utils';
 import http_requests from '../utils/http_requests';
+import { fetchLists } from '../actions/listTemplates';
 //import '../App.css';
 
-const ListTemplates = (props) => {
-  const {listTemplates, updateListTemplates, removeListTemplates} = props;
+class ListTemplates extends Component {
 
-  let listTemplatesAr = []
-  if (listTemplates) {
-    listTemplatesAr = objToArray(listTemplates)
+  state = {
+
   }
 
-  useEffect(() => {
-    //TODO
-    //fetch list template and update hook state
-    let token = props.authenticate && props.authenticate.token ? props.authenticate.token : null;
-    http_requests.Lists.getAllTemplateLists(token)
-      .then(json => updateListTemplates(json))
-      .catch(err => console.error('fetch error', err))
-  });
+  componentDidMount() {
+    this.props.fetchLists();
+  }
 
-  console.log('props', props);
+  removeListTemplates = (ev) => {
+    let listId = ev.target.id;
+    let newListTemplates = {};
 
-  return (
-    <div className="main">
-      <Link to="/settings/listTemplatesNew"><IoIosAddCircleOutline className="list-item-icon-lg" /> New Template List</Link>
-      <Lists lists={listTemplatesAr} type="template" clickHandlerDelete={removeListTemplates} />
-    </div>
-  )
+    //TODO refactor based on redux store for listTemplates
+    //let filteredKeys = Object.keys(listTemplates).filter(k => k !== listId);
+    //filteredKeys.forEach(k => {
+    //  newListTemplates[k] = listTemplates[k];
+    //})
+    //TODO update redux and update BE
+    //setListTemplates(newListTemplates);
+  }
+
+  // const {listTemplates, updateListTemplates, removeListTemplates} = props;
+
+  // if (listTemplates) {
+  //   listTemplatesAr = objToArray(listTemplates)
+  // }
+
+  // useEffect(() => {
+  //   //TODO
+  //   //fetch list template and update hook state
+  //   let token = props.authenticate && props.authenticate.token ? props.authenticate.token : null;
+  //   http_requests.Lists.getAllTemplateLists(token)
+  //     .then(json => updateListTemplates(json))
+  //     .catch(err => console.error('fetch error', err))
+  // });
+
+  // console.log('props', props);
+
+  render() {
+    let listTemplatesAr = []
+
+    return (
+      <div className="main">
+        <Link to="/settings/listTemplatesNew"><IoIosAddCircleOutline className="list-item-icon-lg" /> New Template List</Link>
+        <Lists lists={listTemplatesAr} type="template" clickHandlerDelete={this.removeListTemplates} />
+      </div>
+    )
+  }
 }
+
 
 const mapStateToProps = state => ({ authenticate: state.authenticate });
 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchLists: () => dispatch(fetchLists()),
+  }
+}
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(ListTemplates);
-
-
-//export default ListTemplates;
