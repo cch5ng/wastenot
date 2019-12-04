c8530ccdda9adde1397665aaab7e91cf567aa5d3
cch5ng <cch5ng@gmail.com>
2019-12-04 15:21:44 -0800

    update to use guid instead of id; removed read only links, think it is sufficient to provide edit links

--- client/src/Lists/Lists.js
+++ client/src/Lists/Lists.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import { FaEdit, FaMinusSquareO } from 'react-icons/fa';
 import { IoIosRemoveCircleOutline, IoMdCreate } from "react-icons/io";
 
 const Lists = ({ lists, type, clickHandlerDelete }) => {
   return (
     <ul className="list-group">
       {lists.map(list => {
-        let listLink;
         let editListLink;
 
         if (list) {
           if (type === "shopping") {
-            listLink = `/lists/${list.listId}`;
-            editListLink = `/listEdit/${list.listId}`;
+            editListLink = `/listEdit/${list.guid}`;
           } else if (type === "template") {
-            listLink = `/settings/listTemplates/${list.listId}`;
-            editListLink = `/settings/listTemplatesEdit/${list.listId}`;
+            editListLink = `/settings/listTemplatesEdit/${list.guidd}`;
           }
           return (
             <li key={list.listId}>
-              <Link to={listLink}><span className="list-name">{list.listName}</span></Link>
+              <Link to={editListLink}><span className="list-name">{list.name}</span></Link>
               <Link to={editListLink}><IoMdCreate className="list-item-icon" /></Link>
               <IoIosRemoveCircleOutline className="list-item-icon" id={list.listId} onClick={(ev) => (clickHandlerDelete(ev))} />
             </li>
