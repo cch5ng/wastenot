8e0ecfd77ddb70f6639b601890aa8d6706c84eb0
cch5ng <cch5ng@gmail.com>
2019-12-16 15:46:51 -0800

    add componentDidUpdate() and update fetchLists() to include cookieStr param

--- client/src/ListTemplates/ListTemplates.js
+++ client/src/ListTemplates/ListTemplates.js
@@ -10,7 +10,16 @@ import '../App.css';
 
 class ListTemplates extends Component {
   componentDidMount() {
-    this.props.fetchLists();
+    if (this.props.authenticate && this.props.authenticate.authStr) {
+      this.props.fetchLists({cookie: this.props.authenticate.authStr});
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.authenticate.authStr !== this.props.authenticate.authStr) {
+      this.props.fetchLists({cookie: this.props.authenticate.authStr});
+    }
+
   }
 
   removeListTemplates = (ev) => {
@@ -38,7 +47,7 @@ const mapStateToProps = state => ({ authenticate: state.authenticate, listTempla
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchLists: () => dispatch(fetchLists()),
+    fetchLists: ({cookie}) => dispatch(fetchLists({cookie})),
     fetchTemplateListDelete: (guid) => dispatch(fetchTemplateListDelete(guid))
   }
 }
