0802ca9e0b552b90fe8b2e09a8c1e7b0bd94bef8
cch5ng <cch5ng@gmail.com>
2019-12-17 17:18:14 -0800

    update fetchTemplateListAdd() and fetchTemplateListEdit(); update params to take in cookieStr

--- client/src/actions/listTemplates.js
+++ client/src/actions/listTemplates.js
@@ -86,12 +86,11 @@ export const fetchListTemplate = ({ guid, cookieStr }) => dispatch => {
   }
 }
 
-export const fetchTemplateListAdd = (list) => dispatch => {
+export const fetchTemplateListAdd = ({ list, cookieStr }) => dispatch => {
   dispatch({ type: TEMPLATE_LISTS_ADD_FETCH });
 
-  let cookieStr = getCookieStr();
   if (cookieStr) {
-    http_requests.Lists.postTemplateList(list)
+    http_requests.Lists.postTemplateList({ list, cookieStr })
       .then(resp => {
         if (resp.type === 'error') {
           dispatch({
@@ -119,12 +118,11 @@ export const fetchTemplateListAdd = (list) => dispatch => {
 }
 
 //update a list template
-export const fetchTemplateListEdit = (list) => dispatch => {
+export const fetchTemplateListEdit = ({ list, cookieStr }) => dispatch => {
   dispatch({ type: TEMPLATE_LISTS_FETCH });
 
-  let cookieStr = getCookieStr();
   if (cookieStr) {
-    http_requests.Lists.putTemplateList(list)
+    http_requests.Lists.putTemplateList({ list, cookieStr })
       .then(resp => {
         if (resp.type === 'error') {
           dispatch({
