d4cfab57f2f392d58892ea211fbcc35a75aeaa15
cch5ng <cch5ng@gmail.com>
2019-12-22 20:04:38 -0800

    style header for not authenticated

--- client/src/App/Header/AuthHeader.js
+++ client/src/App/Header/AuthHeader.js
@@ -1,34 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './Header.css';
 import { logout } from '../../actions/authenticate';
 
 const AuthHeader = (props) => {
+  const [menuDisplayed, setMenuDisplayed] = useState(false);
 
   const logOutHandler = (ev) => {
-    //ev.preventDefault();
     props.logout();
   }
 
-  return (
-    <div className="header title">
-      <div className="header-main">
-        <h2><Link to="/" className="header-main">Waste Not</Link></h2>
-      </div>
+  const toggleMenu = (ev) => {
+    console.log('clicked Menu')
+    setMenuDisplayed(!menuDisplayed)
 
-      <div className="header-contact">
-        <p className="nav-link">
-          <Link to="/">Login</Link>
-        </p>
+  }
+
+        // <p className="nav-link">
+        //   <Link to="/">Login</Link>
+        // </p>
+
+  if (!menuDisplayed) {
+    return (
+      <div className="header title">
+        <div className="header-main">
+          <h2><Link to="/" className="title">Waste Not</Link></h2>
+        </div>
+        <div className="header-nav">
+          <p className="action-link">
+            <Link to="/signup">Sign Up</Link>
+          </p>
+          <p className="nav-link" onClick={toggleMenu} >Menu</p>
+        </div>
       </div>
-      <div className="header-contact">
-        <p className="nav-link">
-          <Link to="/register">Register</Link>
-        </p>
+    )
+  }
+
+  if (menuDisplayed) {
+    return (
+      <div className="menu">
+        <div className="icon" onClick={toggleMenu} >X</div>
+        <div className="menu-links">
+          <p className="menu-link-item">
+            <Link to="/">Nav Item</Link>
+          </p>
+          <p className="menu-link-item">
+            <Link to="/">Nav Item2</Link>
+          </p>
+        </div>
+        <div className="menu-buttons">
+          <div className="div-button">
+            <Link to="/signup">Sign </Link>
+          </div>
+          <div className="div-button">
+            <Link to="/">Log In</Link>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 const mapStateToProps = state => ({
