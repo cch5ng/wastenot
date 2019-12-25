41371daa2b3df946c5c7ffff58983c81a7badbbf
cch5ng <cch5ng@gmail.com>
2019-12-24 21:32:26 -0800

    add fetch shopping lists

--- client/src/actions/shoppingLists.js
+++ client/src/actions/shoppingLists.js
@@ -1,71 +1,56 @@
 import http_requests from '../utils/http_requests';
+import { getCookieStr, arrayToObj } from '../utils/utils';
 
 export const SHOPPING_LISTS_FETCH = 'SHOPPING_LISTS_FETCH';
 export const SHOPPING_LISTS_ERR = 'SHOPPING_LISTS_ERR';
 export const SHOPPING_LISTS_FETCH_SUCCESS = 'SHOPPING_LISTS_FETCH_SUCCESS';
 
-// export const REQUEST_LISTS = 'REQUEST_LISTS'
-// export const RECEIVE_LISTS = 'RECEIVE_LISTS'
+// async action for getting posts
+export const fetchShoppingLists = ({ cookieStr }) => dispatch => {
+  dispatch({ type: SHOPPING_LISTS_FETCH });
 
-// export function requestLists() {
+  if (cookieStr) {
+    http_requests.Lists.getAllShoppingLists({ cookieStr })
+      .then(response => {
+        if (response.shoppingLists) {
+          let shoppingListsObj = arrayToObj(response.shoppingLists);
+          dispatch(
+            { type: SHOPPING_LISTS_FETCH_SUCCESS,
+              message: response.message,
+              shoppingLists: shoppingListsObj
+            }
+          )
+        }
+      })
+      .catch(function(err) {
+        dispatch({ type: SHOPPING_LISTS_ERR, message: err.message});
+        console.log('fetch err: ' + err.message)
+      })
+  }
+}
+
+// export const REQUEST_ADD_LIST = 'REQUEST_ADD_LIST'
+// export const RECEIVE_ADD_LIST = 'RECEIVE_ADD_LIST'
+
+// export function requestListCreate() {
 //   return {
-//     type: REQUEST_LISTS,
+//     type: REQUEST_ADD_LIST,
 //     retrieving: true
 //   }
 // }
 
-// export function receiveLists(lists) {
+// export function receiveListCreate(listObj) {
+
+//   let normListObj = {};
+//   let listId = listObj.listId;
+//   normListObj[listId] = listObj;
+
 //   return {
-//     type: RECEIVE_LISTS,
-//     lists,
+//     type: RECEIVE_ADD_LIST,
+//     normListObj,
 //     retrieving: false
 //   }
 // }
-
-// async action for getting posts
-// export const fetchLists = () => dispatch => {
-//   dispatch(requestPosts())
-//   return fetch(API_GET_POSTS, INIT_GET_CATEGORIES)
-//     .then(response => response.json())
-//     // use json.posts to make the data more shallow
-//     .then(json => {
-//       dispatch(receivePosts(json))
-//       // need make multiple calls to get the comments list per postId
-//       let allIds = []
-//       json.forEach(post => {
-//         allIds.push(post.id)
-//       })
-//       allIds.forEach(postId => {
-//         dispatch(fetchComments(postId))
-//       }) 
-//     })
-//     .catch(function(err) {
-//       console.log('fetch err: ' + err.message)
-//     })
-// }
-
-export const REQUEST_ADD_LIST = 'REQUEST_ADD_LIST'
-export const RECEIVE_ADD_LIST = 'RECEIVE_ADD_LIST'
-
-export function requestListCreate() {
-  return {
-    type: REQUEST_ADD_LIST,
-    retrieving: true
-  }
-}
-
-export function receiveListCreate(listObj) {
-
-  let normListObj = {};
-  let listId = listObj.listId;
-  normListObj[listId] = listObj;
-
-  return {
-    type: RECEIVE_ADD_LIST,
-    normListObj,
-    retrieving: false
-  }
-}
 
 // async action for getting posts
 // export const fetchListCreate = (postData) => dispatch => {
@@ -90,27 +75,27 @@ export function receiveListCreate(listObj) {
 /////
 
 // section for edit Post action
-export const REQUEST_EDIT_LIST = 'REQUEST_EDIT_LIST'
-export const RECEIVE_EDIT_LIST = 'RECEIVE_EDIT_LIST'
+// export const REQUEST_EDIT_LIST = 'REQUEST_EDIT_LIST'
+// export const RECEIVE_EDIT_LIST = 'RECEIVE_EDIT_LIST'
 
-export function requestListEdit() {
-  return {
-    type: REQUEST_EDIT_LIST,
-    retrieving: true
-  }
-}
+// export function requestListEdit() {
+//   return {
+//     type: REQUEST_EDIT_LIST,
+//     retrieving: true
+//   }
+// }
 
-export function receiveListEdit(listObj) {
-  let normListObj = {}
-  let listId = listObj.listId
-  normListObj[listId] = listObj
+// export function receiveListEdit(listObj) {
+//   let normListObj = {}
+//   let listId = listObj.listId
+//   normListObj[listId] = listObj
 
-  return {
-    type: RECEIVE_EDIT_LIST,
-    normListObj,
-    retrieving: false
-  }
-}
+//   return {
+//     type: RECEIVE_EDIT_LIST,
+//     normListObj,
+//     retrieving: false
+//   }
+// }
 
 // async action for editing a post
 // export const fetchListEdit = (postId, postData) => dispatch => {
@@ -135,23 +120,23 @@ export function receiveListEdit(listObj) {
 //////
 
 // section for delete Post action
-export const REQUEST_DELETE_LIST = 'REQUEST_DELETE_LIST'
-export const RECEIVE_DELETE_LIST = 'RECEIVE_DELETE_LIST'
+// export const REQUEST_DELETE_LIST = 'REQUEST_DELETE_LIST'
+// export const RECEIVE_DELETE_LIST = 'RECEIVE_DELETE_LIST'
 
-export function requestListDelete() {
-  return {
-    type: REQUEST_DELETE_LIST,
-    retrievingDeletePost: true
-  }
-}
+// export function requestListDelete() {
+//   return {
+//     type: REQUEST_DELETE_LIST,
+//     retrievingDeletePost: true
+//   }
+// }
 
-export function receiveListDelete(listId) {
-  return {
-    type: RECEIVE_DELETE_LIST,
-    retrievingDeleteList: false,
-    listId
-  }
-}
+// export function receiveListDelete(listId) {
+//   return {
+//     type: RECEIVE_DELETE_LIST,
+//     retrievingDeleteList: false,
+//     listId
+//   }
+// }
 
 // async action for editing a post
 // export const fetchListDelete = (postId) => dispatch => {
