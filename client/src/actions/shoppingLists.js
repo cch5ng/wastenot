05d4286dfdcc86b53df2454c6af96f366d1098a1
cch5ng <cch5ng@gmail.com>
2019-12-27 11:28:00 -0800

    add action to handle delete shopping list and update reducer

--- client/src/actions/shoppingLists.js
+++ client/src/actions/shoppingLists.js
@@ -6,6 +6,7 @@ export const SHOPPING_LISTS_ERR = 'SHOPPING_LISTS_ERR';
 export const SHOPPING_LISTS_FETCH_SUCCESS = 'SHOPPING_LISTS_FETCH_SUCCESS';
 export const SHOPPING_LISTS_ADD_FETCH_SUCCESS = 'SHOPPING_LISTS_ADD_FETCH_SUCCESS';
 export const SHOPPING_LISTS_EDIT_FETCH_SUCCESS = 'SHOPPING_LISTS_EDIT_FETCH_SUCCESS';
+export const SHOPPING_LISTS_DELETE_FETCH_SUCCESS = 'SHOPPING_LISTS_DELETE_FETCH_SUCCESS';
 
 export const fetchShoppingLists = ({ cookieStr }) => dispatch => {
   dispatch({ type: SHOPPING_LISTS_FETCH });
@@ -36,7 +37,6 @@ export const fetchShoppingListCreate = ({ list, cookieStr }) => dispatch => {
   if (cookieStr) {
     http_requests.Lists.postShoppingList({ list, cookieStr })
       .then(response => {
-        console.log('response', response)
         if (response.type === 'success') {
           dispatch({
             type: SHOPPING_LISTS_ADD_FETCH_SUCCESS,
@@ -45,8 +45,6 @@ export const fetchShoppingListCreate = ({ list, cookieStr }) => dispatch => {
           })
         }
       })
-      // use json.posts to make the data more shallow
-      // .then(json => )
       .catch(function(err) {
         dispatch({ type: SHOPPING_LISTS_ERR, message: action.message })
         console.log('fetch err: ' + err.message)
@@ -77,88 +75,25 @@ export const fetchShoppingListEdit = ({ list, cookieStr }) => dispatch => {
   }
 }
 
+export const fetchShoppingListDelete = ({ guid, cookieStr }) => dispatch => {
+  dispatch({ type: SHOPPING_LISTS_FETCH });
 
-
-
-/////
-
-// section for edit Post action
-// export const REQUEST_EDIT_LIST = 'REQUEST_EDIT_LIST'
-// export const RECEIVE_EDIT_LIST = 'RECEIVE_EDIT_LIST'
-
-// export function requestListEdit() {
-//   return {
-//     type: REQUEST_EDIT_LIST,
-//     retrieving: true
-//   }
-// }
-
-// export function receiveListEdit(listObj) {
-//   let normListObj = {}
-//   let listId = listObj.listId
-//   normListObj[listId] = listObj
-
-//   return {
-//     type: RECEIVE_EDIT_LIST,
-//     normListObj,
-//     retrieving: false
-//   }
-// }
-
-// async action for editing a post
-// export const fetchListEdit = (postId, postData) => dispatch => {
-//   dispatch(requestPostEdit())
-//   let INIT_EDIT_POST = {method: 'PUT',
-//                           headers: {
-//                             'Authorization': 'mAuth',
-//                             "Content-Type": 'application/json'
-//                           },
-//                           body: JSON.stringify(postData)
-//                         }
-
-//   return fetch(`${API_GET_POSTS}/${postId}`, INIT_EDIT_POST)
-//     .then(response => response.json())
-//     // use json.posts to make the data more shallow
-//     .then(json => dispatch(receivePostEdit(json)))
-//     .catch(function(err) {
-//       console.log('fetch err: ' + err.message)
-//     })
-// }
-
-//////
-
-// section for delete Post action
-// export const REQUEST_DELETE_LIST = 'REQUEST_DELETE_LIST'
-// export const RECEIVE_DELETE_LIST = 'RECEIVE_DELETE_LIST'
-
-// export function requestListDelete() {
-//   return {
-//     type: REQUEST_DELETE_LIST,
-//     retrievingDeletePost: true
-//   }
-// }
-
-// export function receiveListDelete(listId) {
-//   return {
-//     type: RECEIVE_DELETE_LIST,
-//     retrievingDeleteList: false,
-//     listId
-//   }
-// }
-
-// async action for editing a post
-// export const fetchListDelete = (postId) => dispatch => {
-//   dispatch(requestPostDelete())
-//   let INIT_EDIT_POST = {method: 'DELETE',
-//                           headers: {
-//                             'Authorization': 'mAuth',
-//                             "Content-Type": 'application/json'
-//                           }
-//                         }
-
-//   return fetch(`${API_GET_POSTS}/${postId}`, INIT_EDIT_POST)
-//     .then(() => dispatch(receivePostDelete(postId)))
-//     .catch(function(err) {
-//       console.log('fetch err: ' + err.message)
-//     })
-// }
+  if (cookieStr) {
+    http_requests.Lists.deleteTemplateList({ guid, cookieStr })
+      .then(response => {
+        if (response.type === 'success') {
+          //let shoppingListsObj = arrayToObj(response.templateList);
+          dispatch(
+            { type: SHOPPING_LISTS_DELETE_FETCH_SUCCESS,
+              message: response.message,
+              deletedGuid: response.guid
+            }
+          )
+        }
+      })
+      .catch(function(err) {
+        dispatch({ type: SHOPPING_LISTS_ERR, message: err.message});
+        console.log('fetch err: ' + err.message)
+      })
+  }
+}
