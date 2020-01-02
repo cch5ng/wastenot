f06dc97d2c3efd5ee77d0ff004edb4651fbe987a
cch5ng <cch5ng@gmail.com>
2020-01-01 18:22:24 -0800

    store list item with sort order; return list items sorted by sort order

--- server/app/list_item/table.js
+++ server/app/list_item/table.js
@@ -4,98 +4,98 @@ const { section_name_to_id } = require('../utils/constants');
 
 class ListItemTable {
 
-	//REFACTOR, maybe should move to ListItemTable class
-	static storeListItem({ name, list_guid, list_item_guid }) {
-		return new Promise((resolve, reject) => {
-			pool.query(
-				`INSERT INTO list_item (name, list_guid, guid) VALUES ($1, $2, $3) RETURNING list_guid`,
-				[name, list_guid, list_item_guid],
-				(error, response) => {
-					if (error) return reject(error);
-					if (response.rows.length) {
-						resolve({ list_guid });
-					}
-				}
-			)
-		})
-	}
+  //REFACTOR, maybe should move to ListItemTable class
+  static storeListItem({ name, list_guid, list_item_guid, sortOrder }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `INSERT INTO list_item (name, list_guid, guid, sort_order) VALUES ($1, $2, $3, $4) RETURNING list_guid`,
+        [name, list_guid, list_item_guid, sortOrder],
+        (error, response) => {
+          if (error) return reject(error);
+          if (response.rows.length) {
+            resolve({ list_guid });
+          }
+        }
+      )
+    })
+  }
 
-	//probably want to get everything like name plus the list items
-	static getListItemsByListGuid({ listGuid }) {
-		return new Promise((resolve, reject) => {
-			pool.query(
-				`SELECT list_item.guid, list_item.name, list_item.sort_order, list_item.list_guid from list_item WHERE list_guid = $1`,
-				[listGuid],
-				(error, response) => {
-					if (error) return reject(error);
-					if (response.rows.length) {
-						resolve({listItems: response.rows});
-					}
-				}
-			)
-		})
-	}
+  //probably want to get everything like name plus the list items
+  static getListItemsByListGuid({ listGuid }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT list_item.guid, list_item.name, list_item.sort_order, list_item.list_guid from list_item WHERE list_guid = $1 ORDER BY list_item.sort_order`,
+        [listGuid],
+        (error, response) => {
+          if (error) return reject(error);
+          if (response.rows.length) {
+            resolve({listItems: response.rows});
+          }
+        }
+      )
+    })
+  }
 
-	static updateListItem({ name, guid, sort_order }) {
-		//const { guid, name, sort_order } = listItem;
-		return new Promise((resolve, reject) => {
-			pool.query(
-				`UPDATE list_item SET name = $2, sort_order = $3 WHERE guid = $1 RETURNING guid`,
-				[guid, name, sort_order],
-				(error, response) => {
-					if (error) return reject(error);
-					if (response.rows.length) {
-						resolve({list_item_guid: response.rows[0].guid});
-					}
-				}
-			)
-		})
-	}
+  static updateListItem({ name, guid, sort_order }) {
+    //const { guid, name, sort_order } = listItem;
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `UPDATE list_item SET name = $2, sort_order = $3 WHERE guid = $1 RETURNING guid`,
+        [guid, name, sort_order],
+        (error, response) => {
+          if (error) return reject(error);
+          if (response.rows.length) {
+            resolve({list_item_guid: response.rows[0].guid});
+          }
+        }
+      )
+    })
+  }
 
-	static updateListItems(listItems) {
-		return Promise.all(
-			listItems.map(listItem => {
-				const { name, guid, sort_order } = listItem;
-				return ListItemTable.updateListItem({ name, guid, sort_order })
-			})
-		)
-	}
+  static updateListItems(listItems) {
+    return Promise.all(
+      listItems.map(listItem => {
+        const { name, guid, sort_order } = listItem;
+        return ListItemTable.updateListItem({ name, guid, sort_order })
+      })
+    )
+  }
 
-	static deleteListItemByItemGuid(itemGuid) {
-		return new Promise((resolve, reject) => {
-			pool.query(
-				`DELETE from list_item WHERE guid = $1 RETURNING guid`,
-				[itemGuid],
-				(error, response) => {
-					if (error) return reject(error);
-					if (response.rows.length) {
-						resolve({list_item_guid: response.rows[0].guid})
-					}
-				}
-			)
-		})
-	}
+  static deleteListItemByItemGuid(itemGuid) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `DELETE from list_item WHERE guid = $1 RETURNING guid`,
+        [itemGuid],
+        (error, response) => {
+          if (error) return reject(error);
+          if (response.rows.length) {
+            resolve({list_item_guid: response.rows[0].guid})
+          }
+        }
+      )
+    })
+  }
 
-	// static deleteListItemsByItemGuid(itemGuids) {
-	// 	return Promise.all(
-	// 		itemGuids.map(guid => ListItemTable.deleteListItemByItemGuid(guid))
-	// 	)
-	// }
+  // static deleteListItemsByItemGuid(itemGuids) {
+  //  return Promise.all(
+  //    itemGuids.map(guid => ListItemTable.deleteListItemByItemGuid(guid))
+  //  )
+  // }
 
-	static deleteListItemByListGuid(guid) {
-		return new Promise((resolve, reject) => {
-			pool.query(
-				`DELETE from list_item WHERE list_guid = $1 RETURNING guid`,
-				[guid],
-				(error, response) => {
-					if (error) return reject(error);
-					if (response.rows.length) {
-						resolve({list_item_guid: response.rows[0].guid});
-					}
-				}
-			)
-		})
-	}
+  static deleteListItemByListGuid(guid) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `DELETE from list_item WHERE list_guid = $1 RETURNING guid`,
+        [guid],
+        (error, response) => {
+          if (error) return reject(error);
+          if (response.rows.length) {
+            resolve({list_item_guid: response.rows[0].guid});
+          }
+        }
+      )
+    })
+  }
 
 }
 
