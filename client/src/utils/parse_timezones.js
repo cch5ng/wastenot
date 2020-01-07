6e7796627acbfa9634d46635b3ae791169b281f8
cch5ng <cch5ng@gmail.com>
2020-01-07 14:38:27 -0800

    cleanup

--- client/src/utils/parse_timezones.js
+++ client/src/utils/parse_timezones.js
@@ -10,15 +10,6 @@ fs.writeFile('db_timezone.js', 'let dbTimezoneAr = [', (err) => {
     console.log('db_timezone.js started');
 });
 
-// let writeStream = fs.createWriteStream('db_timezone.js');
-
-// // write some data with a base64 encoding
-// writeStream.write('let dbTimezoneAr = [', 'utf8');
-
-// the finish event is emitted when all data has been flushed from the stream
-
-//////
-
 const readInterface = readline.createInterface({
   input: fs.createReadStream('zone1970.tab'),
   output: process.stdout,
@@ -28,8 +19,6 @@ const readInterface = readline.createInterface({
 readInterface.on('line', function(line) {
   let lineAr = [];
   lineAr = line.split('\t');
-  //console.log('lineAr', lineAr)
-  //console.log('len lineAr', lineAr.length)
   if (lineAr[0].indexOf('#') !== 0) {
     timeZoneAr.push(lineAr[2]);
 
@@ -38,28 +27,17 @@ readInterface.on('line', function(line) {
         console.log('db_timezone.js was updated!');
     });
 
-    //writeStream.write(lineAr[2], 'utf8');
   }
 });
 
 readInterface.on('close', function() {
-  //writeStream.write('];', 'utf8');
     fs.appendFile('db_timezone.js', '];\n export default dbTimezoneAr;', (err) => {
         if (err) throw err;
         console.log('The lyrics were updated!');
     });
-})
+});
 
 
 readInterface.on('finish', function() {
   console.log('timeZoneAr', timeZoneAr);
-})
-
-// writeStream.on('finish', () => {
-//     console.log('wrote all data to file');
-// });
-
-// // close the stream
-// writeStream.end();
-
-
+});
