5a4208b32456d8228f9047e6d56028d1ca92a9a6
cch5ng <cch5ng@gmail.com>
2020-01-09 11:24:08 -0800

    add timezone col

--- server/bin/sql/list_item.sql
+++ server/bin/sql/list_item.sql
@@ -6,6 +6,7 @@ CREATE TABLE list_item(
   guid VARCHAR(64) UNIQUE,
   section_id INTEGER,
   checked BOOLEAN,
+  timestamp TIMESTAMPTZ,
   FOREIGN KEY (list_guid) REFERENCES list(guid),
   FOREIGN KEY (section_id) REFERENCES section(id)
 );
