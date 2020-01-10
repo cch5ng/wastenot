74223dc383a0361169208caa08c3a5870f15c307
cch5ng <cch5ng@gmail.com>
2020-01-10 11:22:37 -0800

    fix issue where the default timezone (already saved in BE) was not displaying correctly

--- client/src/Auth/Settings.js
+++ client/src/Auth/Settings.js
@@ -4,10 +4,11 @@ import Select from 'react-select';
 import Button from '../App/Shared/Button/Button';
 import InputText from '../App/Shared/InputText/InputText';
 import dbTimezoneAr from '../utils/db_timezone';
+import http_requests from '../utils/http_requests';
 
 const Settings = (props) => {
 
-  const [timezone, setTimezone] = useState('none');
+  const [timezone, setTimezone] = useState({value: 'none', label: 'Select a local timezone'});
 
   function onChangeHandler(selectedOption) {
     setTimezone(selectedOption);
@@ -15,11 +16,21 @@ const Settings = (props) => {
 
   function formSubmitHandler(ev) {
     ev.preventDefault();
-    console.log('todo make BE request to update timezone')
+    if (props.authenticate && props.authenticate.authStr) {
+      http_requests.Auth.putTimezone({timezone, cookieStr: props.authenticate.authStr})
+    }
   }
 
   useEffect(() => {
     //get the user's timezone and if it exists set the timezone
+    if (props.authenticate && props.authenticate.authStr) {
+      http_requests.Auth.getTimezone({cookieStr: props.authenticate.authStr})
+        .then((resp) => {
+          if (resp.time_zone.time_zone) {
+            setTimezone(JSON.parse(resp.time_zone.time_zone));
+          }
+        })
+    }
   }, [])
 
   //reformat the data source for consumption by react select
