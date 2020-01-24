ac6c425d7ad59d4058da6508a19a4a95639b76e6
cch5ng <cch5ng@gmail.com>
2020-01-24 13:44:21 -0800

    stub out test for getSubstringMatchCount()

--- client/src/utils/map_expiration_dates.test.js
+++ client/src/utils/map_expiration_dates.test.js
@@ -4,10 +4,38 @@ const {
   getExpirationDate
 } = require('./map_expiration_dates');
 
-test('dummy test removePunctuation', () => {
-  expect(removePunctuation('lunch meats, opened')).toBe('lunch meats opened');
-});
+describe('map expiration dates suite tests', () => {
 
-test('dummy test2 removePunctuation', () => {
-  expect(removePunctuation('lunch meats, opened, or closed')).toBe('lunch meats opened or closed');
+  describe('removePunctuation tests', () => {
+    test('removePunctuation() function test with one comma', () => {
+      expect(removePunctuation('lunch meats, opened')).toBe('lunch meats opened');
+    });
+
+    test('removePunctuation() function test with two commas', () => {
+      expect(removePunctuation('lunch meats, opened, or closed')).toBe('lunch meats opened or closed');
+    });
+  })
+
+  describe('getSubstringMatchCount', () => {
+    test('getSubstringMatchCount() function test', () => {
+      const ar1 = ['a', 'b', 'c', 'd', 'e'];
+      const ar2 = ['z', 'y', 'e', 'c', 'a', 'o', 'p', 'd'];
+      expect(getSubstringMatchCount(ar1, ar2)).toBe(4);
+    })
+
+    test('getSubstringMatchCount() function test all match', () => {
+      const ar3 = ['a', 'b', 'c', 'd', 'e'];
+      const ar4 = ['a', 'b', 'c', 'd', 'e'];
+      expect(getSubstringMatchCount(ar3, ar4)).toBe(5);
+    })
+
+    test('getSubstringMatchCount() function test no match', () => {
+      const ar5 = ['a', 'b', 'c', 'd', 'e'];
+      const ar6 = ['z', 'y', 'o', 'p', 'm'];
+      expect(getSubstringMatchCount(ar5, ar6)).toBe(0);
+    })
+  })
+
+
+
 });
