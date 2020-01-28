cb835686d3f758e1336be222e4ce29c1194ad559
cch5ng <cch5ng@gmail.com>
2020-01-27 16:10:29 -0800

    stub out initial ExpirationMapTest but need to figure out the details of how to render the form; then how to track changes to form in state; also how to use the mapping logic

---
+++ client/src/Settings/ExpirationMapTest.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Checkbox from '../App/Shared/Checkbox';
+import Button from '../App/Shared/Button';
+import InputText from '../App/Shared/InputText';
+import SelectList from '../App/Shared/SelectList';
+
+const ExpirationMapTest = (props) => {
+
+  const [mappings, setMappings] = useState({});
+
+  renderForm = () => {
+
+    return (
+      for (let i = 0; i < 29; i++) {
+        <div>
+          <checkbox>
+          <input>
+          <react select>
+        </div>
+      }
+    )
+
+  }
+
+  return (
+    <div>
+      <form>
+      </form>
+    </div>
+  )
+
+}
+
+export default ExpirationMapTest;
