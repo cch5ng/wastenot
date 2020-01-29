3d7282ce0b2d18990f2b06403748b7e1b814e929
cch5ng <cch5ng@gmail.com>
2020-01-29 15:50:29 -0800

    add getExpirationCategory() and refactor getExpirationDate()

--- client/src/utils/map_expiration_dates.js
+++ client/src/utils/map_expiration_dates.js
@@ -24,7 +24,7 @@ const getSubstringMatchCount = (ar1, ar2) => {
   return matchCnt;
 }
 
-const getExpirationDate = (item, EXPIRATION_DATES) => {
+const getExpirationCategory = (item, EXPIRATION_DATES) => {
   let expirationKeys = Object.keys(EXPIRATION_DATES);
   let itemNoPunctuation;
   let highestSubstringMatchCount = 0;
@@ -56,17 +56,56 @@ const getExpirationDate = (item, EXPIRATION_DATES) => {
 
   if (highestSubstringMatchIdx) {
     key = expirationKeys[highestSubstringMatchIdx];
-    return EXPIRATION_DATES[key].refrigeratorInt;
+    return key;
   } else {
     return null;
   }
 }
 
-const getExpirationCategory = (item, EXPIRATION_DATES) => {
+const getExpirationDate = (item, EXPIRATION_DATES) => {
+  // let expirationKeys = Object.keys(EXPIRATION_DATES);
+  // let itemNoPunctuation;
+  // let highestSubstringMatchCount = 0;
+  // let highestSubstringMatchIdx;
+  let key = getExpirationCategory(item, EXPIRATION_DATES);
+  if (key) {
+    return EXPIRATION_DATES[key].refrigeratorInt;
+  }
+  return 'none';
 
+  //case exact string match
+  // if (expirationKeys.indexOf(item) > -1) {
+  //   let idx = expirationKeys.indexOf(item);
+  //   key = expirationKeys[idx];
+  //   return EXPIRATION_DATES[key].refrigeratorInt;
+  // }
+
+  //highest substring match (does this need to be in order or just a combination)
+  //get 2 arrays where each string has been parsed using string.split('') but what to do about punctuation
+  //remove punctuation
+  //split string into array of indiv words
+  // itemNoPunctuation = removePunctuation(item);
+  // let itemWordAr = itemNoPunctuation.split(' ');
+  // for (let i = 0; i < expirationKeys.length; i++) {
+  //   let expirationKeyNoPunctuation = removePunctuation(expirationKeys[i]);
+  //   let expirationKeyWordAr = expirationKeyNoPunctuation.split(' ');
+  //   let substringMatchCount = getSubstringMatchCount(itemWordAr, expirationKeyWordAr);
+  //   if (substringMatchCount > highestSubstringMatchCount) {
+  //     highestSubstringMatchCount = substringMatchCount;
+  //     highestSubstringMatchIdx = i;
+  //   }
+  // }
+
+  // if (highestSubstringMatchIdx) {
+  //   key = expirationKeys[highestSubstringMatchIdx];
+  //   return EXPIRATION_DATES[key].refrigeratorInt;
+  // } else {
+  //   return null;
+  // }
 }
 
-module.exports = {
+//module.exports = {
+export {
   removePunctuation,
   getSubstringMatchCount,
   getExpirationDate,
