54e1b8050cbc0989fccf236309b99af29a56e6f0
Carol Chung <cch5ng@gmail.com>
2020-02-23 12:21:11 -0800

    refactor to set link onclick handler to hide notification

--- client/src/App/InternalNotification/InternalNotification.js
+++ client/src/App/InternalNotification/InternalNotification.js
@@ -5,25 +5,12 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 const InternalNotification = (props) => {
-
   return (
     <div className="internalNotification">
-      This is a reminder to <Link to="/settings/expirationMapTest">set up mapping between your shopping list items and expiration dates.</Link>
+      This is a reminder to <Link to="/settings/expirationMapTest" onClick={props.clickCloseHandler}>set up mapping between your shopping list items and expiration dates.</Link>
       <div onClick={props.clickCloseHandler}>X</div>
     </div>
   )
 }
 
 export default InternalNotification;
-
-// const mapStateToProps = state => ({ setting: state.setting });
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     //register: ({ email, password }) => dispatch(register({ email, password }))
-//   }
-// }
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps)(InternalNotification);
