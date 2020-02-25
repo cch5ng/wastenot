75caef87d4bb1ce0e092fe3a6a565f32d74412b5
Carol Chung <cch5ng@gmail.com>
2020-02-25 15:19:56 -0800

    add notificationCloseGlobalHandler() for user by notification checkbox

--- client/src/App/InternalNotification/InternalNotification.js
+++ client/src/App/InternalNotification/InternalNotification.js
@@ -3,12 +3,16 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './style.css';
+import Checkbox from '../Shared/Checkbox/Checkbox';
 
 const InternalNotification = (props) => {
   return (
     <div className="internalNotification">
-      This is a reminder to <Link to="/settings/expirationMapTest" onClick={props.clickCloseHandler}>set up mapping between your shopping list items and expiration dates.</Link>
       <div onClick={props.clickCloseHandler}>X</div>
+      <p>This is a reminder to <Link to="/settings/expirationMapTest" onClick={props.clickCloseHandler}>set up mapping between your shopping list items and expiration dates.</Link></p>
+      <form>
+        <Checkbox checkboxVal="false" checkboxLabel="Hide this message in the future" onChangeHandler={props.checkboxSelectHandler} />
+      </form>
     </div>
   )
 }
