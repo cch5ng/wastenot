a2eba34ebf0c580b4a0a830547ad8e56d2ce24e1
Carol Chung <cch5ng@gmail.com>
2020-02-26 15:59:16 -0800

    refactor to add grid styling option

--- client/src/App/Shared/InputText/InputText.js
+++ client/src/App/Shared/InputText/InputText.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 import './style.css';
 import '../../../App.css';
 
 const InputText = (props) => {
-  let {value, options, placeholder, id, onChangeHandler, name} = props;
+  let {value, options, placeholder, id, onChangeHandler, name, inputClassName } = props;
+  let inputClass;
+
+  if (inputClassName) {
+    inputClass = `formGroup ${inputClassName}`
+  }
 
   return (
-    <div className="formGroup">
-      <input type="text" className="formGroup" id={id} value={value} 
+    <div className={inputClass}>
+      <input type="text" className={inputClass} id={id} value={value} 
         onChange={ev => onChangeHandler(ev)}
         placeholder={placeholder} name={name} />
     </div>
