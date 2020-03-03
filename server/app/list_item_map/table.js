95d7bfddc97784f791d3a4efd75e3de7893e4ab4
Carol Chung <cch5ng@gmail.com>
2020-03-02 16:25:21 -0800

    modify list item map table so another col, skip_notification, is added; modify post request to include this field

--- server/app/list_item_map/table.js
+++ server/app/list_item_map/table.js
@@ -5,10 +5,10 @@ class ListItemMapTable {
   // list items for template list
   static storeListItemMap({ listItemMap, user_id }) {
     return new Promise((resolve, reject) => {
-      const { name, expirationDays } = listItemMap;
+      const { name, expirationDays, skipNotification } = listItemMap;
       pool.query(
-        `INSERT INTO list_item_map (name, expiration_days, user_id) VALUES ($1, $2, $3) RETURNING name`,
-        [name, expirationDays, user_id],
+        `INSERT INTO list_item_map (name, expiration_days, user_id, skip_notification) VALUES ($1, $2, $3, $4) RETURNING name`,
+        [name, expirationDays, user_id, skipNotification],
         (error, response) => {
           if (error) return reject(error);
           if (response.rows.length) {
