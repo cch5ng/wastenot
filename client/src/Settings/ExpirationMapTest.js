fdc6b509d58d18b6d93c7aaccf6dbaaa3d5ace2c
Carol Chung <cch5ng@gmail.com>
2020-03-02 16:23:51 -0800

    update stepTwoFormSubmitHandler() to handle skipNotification prop; cleanup

--- client/src/Settings/ExpirationMapTest.js
+++ client/src/Settings/ExpirationMapTest.js
@@ -112,12 +112,11 @@ const ExpirationMapTest = (props) => {
 
     let cookie = getCookieStr();
     mappings.forEach(item => {
-      if (item.disabled === false && item.text.length && item.expirationCategory.length) {
-        console.log('gets here')
-        console.log('item.text', item.text)
+      if (item.text.length && item.expirationCategory.length) {
         let obj = {}
         obj.name = item.text;
         obj.expirationDays = getExpirationDate(item.expirationCategory, EXPIRATION_DATES);
+        obj.skipNotification = item.disabled;
         mappingsWithExpiration.push(obj);
       }
     });
@@ -127,28 +126,7 @@ const ExpirationMapTest = (props) => {
       })
       .catch(err => console.error('err', err));
     ;
-    //console.log('mappingsWithExpiration', mappingsWithExpiration);
-
-
-    // let mappedExpirationCategoriesAr = getMappedExpirationCategoriesAr();
-    // let newMappings = [].concat(mappings);
-    // mappedExpirationCategoriesAr.forEach((expirCategory, idx) => {
-    //   newMappings[idx].expirationCategory = expirCategory;
-    // })
-    // setMappings(newMappings);
-    // http_requests.Setting.putListItemMapSetting(cookie)
-    //   .then(resp => console.log('resp', resp))
-    //   .catch(err => console.error(err))
   }
-
-
-  //determines how to convert above mapping to a resulting expiration date duration
-  //I think this does not affect the UI but should be stored into state/backend
-  //const expirationDateMapper2 = () => {
-  //}
-
-  //const renderForm = () => {
-  //}
 
   if (expirationMapPage === 1) {
     return (
@@ -172,15 +150,9 @@ const ExpirationMapTest = (props) => {
 export default ExpirationMapTest;
 
 const MappingStep1 = (props) => {
-
-//        <div className="twoCol">Expiration Category</div>
-//inputClassName="twoCol"
-//              <SelectList value={m.expirationCategory} options={props.expirationCategoryOptions} 
-//onChange={props.selectListChangeHandler} id={selectListIdx} selectClassName="twoCol"/>
-
   return (
     <div>
-      <h1>TODO need explanation purpose of this form</h1>
+      <h1>Step 1: Enter a Sample Shopping List</h1>
       <div className="row">
         <div className="oneCol">List Item</div>
       </div>
@@ -207,7 +179,10 @@ const MappingStep1 = (props) => {
 const ReviewStep2 = (props) => {
   return (
     <div>
-      <h1>TODO need explanation REVIEW step</h1>
+      <h1>Step 2: Review Mappings</h1>
+      <p>
+        The system has automatically found food categories that match with the shopping list items. You can choose to skip getting notifications for an item by clicking its checkbox. If the food category does not look right, you can select a different category from the drop-down list or select "no match" if there is no corresponding category.
+      </p>
       <div className="row">
         <div className="sixCol">Disable</div>
         <div className="threeCol">List Item</div>
@@ -225,7 +200,7 @@ const ReviewStep2 = (props) => {
               <Checkbox checkboxVal={m.disabled} onChangeHandler={props.checkboxChangeHandler} 
                 name="checkbox" id={checkboxIdx} checkClassName="sixCol" />
               <InputText value={m.text} onChangeHandler={props.inputChangeHandler} 
-                name="inputText" id={inputTextIdx} inputClassName="threeCol"/>
+                name="inputText" id={inputTextIdx} inputClassName="threeCol" readOnly={true} />
               <SelectList value={m.expirationCategory} options={props.expirationCategoryOptions} 
                 onChange={props.selectListChangeHandler} id={selectListIdx} selectClassName="threeCol"/>
             </div>
