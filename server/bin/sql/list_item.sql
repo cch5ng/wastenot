72ae3b8a43885df0708e98808f32d6f85e994aae
Carol Chung <cch5ng@gmail.com>
2020-03-13 21:55:23 -0700

    refactor list_item table; add column to ref list_item_map_id

--- server/bin/sql/list_item.sql
+++ server/bin/sql/list_item.sql
@@ -6,7 +6,9 @@ CREATE TABLE list_item(
   guid VARCHAR(64) UNIQUE,
   section_id INTEGER,
   checked BOOLEAN,
+  list_item_map_id INTEGER,
   timestamp TIMESTAMPTZ,
   FOREIGN KEY (list_guid) REFERENCES list(guid),
-  FOREIGN KEY (section_id) REFERENCES section(id)
+  FOREIGN KEY (section_id) REFERENCES section(id),
+  FOREIGN KEY (list_item_map_id) REFERENCES list_item_map(id)
 );
