830a9c1a8d9f5865f8a6f758aa61ffb7dcb17d5e
Carol Chung <cch5ng@gmail.com>
2020-03-16 12:29:01 -0700

    update list item map store proc to include guid val

--- server/app/list_item_map/table.js
+++ server/app/list_item_map/table.js
@@ -6,9 +6,10 @@ class ListItemMapTable {
   static storeListItemMap({ listItemMap, user_id }) {
     return new Promise((resolve, reject) => {
       const { name, expirationDays, skipNotification } = listItemMap;
+      const guid = uuidv4();
       pool.query(
-        `INSERT INTO list_item_map (name, expiration_days, user_id, skip_notification) VALUES ($1, $2, $3, $4) RETURNING name`,
-        [name, expirationDays, user_id, skipNotification],
+        `INSERT INTO list_item_map (name, expiration_days, user_id, skip_notification, guid) VALUES ($1, $2, $3, $4, $5) RETURNING name`,
+        [name, expirationDays, user_id, skipNotification, guid],
         (error, response) => {
           if (error) return reject(error);
           if (response.rows.length) {
