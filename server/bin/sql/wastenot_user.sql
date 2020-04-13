fc6dfd6d619a3dc8e6957609c9ed38903fe45891
Carol Chung <cch5ng@gmail.com>
2020-04-12 20:05:15 -0700

    add wastenot_user col, push_subscription

--- server/bin/sql/wastenot_user.sql
+++ server/bin/sql/wastenot_user.sql
@@ -1,10 +1,11 @@
 CREATE TABLE wastenot_user(
-  id             SERIAL PRIMARY KEY,
-  "emailHash"    VARCHAR(64) UNIQUE,
-  "passwordHash" VARCHAR(64),
-  name           VARCHAR(64),
-  created_at     TIMESTAMP NOT NULL DEFAULT NOW(),
-  guid           VARCHAR(64) UNIQUE,
-  "sessionId"    VARCHAR(36),
-  time_zone      VARCHAR(255)
+  id                SERIAL PRIMARY KEY,
+  "emailHash"       VARCHAR(64) UNIQUE,
+  "passwordHash"    VARCHAR(64),
+  name              VARCHAR(64),
+  created_at        TIMESTAMP NOT NULL DEFAULT NOW(),
+  guid              VARCHAR(64) UNIQUE,
+  "sessionId"       VARCHAR(36),
+  time_zone         VARCHAR(255),
+  push_subscription VARCHAR(1024)
 );
