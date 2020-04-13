bddf5f2185fc57f04704ca185e4f748d955f7884
Carol Chung <cch5ng@gmail.com>
2020-04-12 20:10:45 -0700

    start to chain some functions like registration, request push permission, save push subscription

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,12 +1,18 @@
-//sw.js
+import http_requests from './utils/http_requests';
 
 //workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+let curRegistration;
+let curPushSubscription;
 
 export function registerServiceWorker() {
     return navigator.serviceWorker.register('./sw.js')
     .then(function(registration) {
         console.log('Service worker successfully registered.');
+        curRegistration = registration
         return registration;
+    })
+    .then(registration => {
+        askPermission();
     })
     .catch(function(err) {
         console.error('Unable to register service worker.', err);
@@ -25,10 +31,75 @@ function askPermission() {
         }
     })
     .then(function(permissionResult) {
+        console.log('permissionResult', permissionResult)
         if (permissionResult !== 'granted') {
-        throw new Error('We weren\'t granted permission.');
+            throw new Error('We weren\'t granted permission.');
+        } else {
+            subscribeUserToPush();
         }
     });
+}
+
+export function subscribeUserToPush() {
+    return navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      const subscribeOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          'BDjo9Vgy4AEijfm0a0Re38fUSLLsJzn5l-4tVnbLFfSJsR0_iWL2qUT76hPYZifqx7t3djR1Xc4gMCPM0Yn2PU8'
+        )
+      };
+  
+      return registration.pushManager.subscribe(subscribeOptions);
+    })
+    .then(function(pushSubscription) {
+      console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+      curPushSubscription = pushSubscription;
+
+      let sessionStr = sessionStorage.getItem('sessionStr');
+      let sessionStrAr;
+      if (sessionStr) {
+          sessionStrAr = sessionStr.split('|');
+          let email = sessionStrAr[0];
+          http_requests.Auth.postPushSubscription({email, pushSubscription: JSON.stringify(pushSubscription)})
+
+      } else {
+          alert('user is not logged in so push subscription cannotbe saved')
+      }
+      return pushSubscription;
+    });
+  }
+
+export function unsubscribeUserFromPush() {
+    if (curPushSubscription) {
+        curPushSubscription.unsubscribe()
+            .then(successful => {
+                if (successful) {
+                    console.log('pushSubscription was unsubscribed');
+                }
+            })
+            .catch(err => console.error(`error: ${err}; pushSubscription could not be unsubscribed`))
+    }
+}
+
+/**
+ * urlBase64ToUint8Array
+ * 
+ * @param {string} base64String a public vavid key
+ */
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
 
 // export {
