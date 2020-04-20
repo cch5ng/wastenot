072c61e1db12141259e5686f56e61fcf1a6c085e
Carol Chung <cch5ng@gmail.com>
2020-04-20 13:55:07 -0700

    switched order of table creation to avoid dependency issue

--- server/bin/configure_db.sh
+++ server/bin/configure_db.sh
@@ -12,9 +12,10 @@ createdb -U ${DB_USER} waste_not_db
 psql -U ${DB_USER} waste_not_db < ./bin/sql/wastenot_user.sql
 psql -U ${DB_USER} waste_not_db < ./bin/sql/list.sql
 psql -U ${DB_USER} waste_not_db < ./bin/sql/section.sql
+psql -U ${DB_USER} waste_not_db < ./bin/sql/list_item_map.sql
 psql -U ${DB_USER} waste_not_db < ./bin/sql/list_item.sql
 psql -U ${DB_USER} waste_not_db < ./bin/sql/expiration.sql
 psql -U ${DB_USER} waste_not_db < ./bin/sql/setting.sql
-psql -U ${DB_USER} waste_not_db < ./bin/sql/list_item_map.sql
+psql -U ${DB_USER} waste_not_db < ./bin/sql/notification.sql
 
 echo 'waste_not_db configured'
