879ef48dd57bda71081a9b6caa94c7f9dfc3f39f
Carol Chung <cch5ng@gmail.com>
2020-04-30 20:57:25 -0700

    remove notification table be the data was included with line_item

--- server/bin/sql/notification.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE notification(
-  id SERIAL PRIMARY KEY,
-  to_send_timestamp TIMESTAMPTZ,
-  sent_timestamp TIMESTAMPTZ,
-  list_item_id INTEGER,
-  list_item_map_id INTEGER,
-  FOREIGN KEY (list_item_map_id) REFERENCES list_item_map(id),
-  FOREIGN KEY (list_item_id) REFERENCES list_item(id)
-);
