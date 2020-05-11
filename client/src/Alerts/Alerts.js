7dba57bc1a2e248683bc714172154e06d2dd0adf
Carol Chung <cch5ng@gmail.com>
2020-05-10 22:06:10 -0700

    modify alerts style

--- client/src/Alerts/Alerts.js
+++ client/src/Alerts/Alerts.js
@@ -96,8 +96,9 @@ const Alerts = (props) => {
                 (<div>
                     <p>{alert.name} will expire on {alert.notify_timestamp}</p>
                     <Button onClickHandler={handlePostponeAlert} label="Postpone (1 day)" classVal={alert.guid}
-                        size="medium" />
-                    <Button onClickHandler={handleCancelAlert} label="Cancel alert" classVal={alert.guid}/>
+                        size="medium" color="light" />
+                    <Button onClickHandler={handleCancelAlert} label="Cancel alert" classVal={alert.guid}
+                         color="light"/>
                 </div>)
             )}
 
