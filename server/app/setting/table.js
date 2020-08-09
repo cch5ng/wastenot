5f21d22ef91753ec63a39ac23f46cc6153becc01
Carol Chung <cch5ng@gmail.com>
2020-08-08 23:04:52 -0700

    modify storeMappedItemsSetting

--- server/app/setting/table.js
+++ server/app/setting/table.js
@@ -1,55 +1,81 @@
+const {Client} = require('pg');
 const pool = require('../../databasePool');
 
 class SettingTable {
 
-    static storeMappedItemsSetting({ user_id }) {
-        return new Promise((resolve, reject) => {
-            pool.query(
-                `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
-                [user_id],
-                (error, response) => {
-                  if (error) return reject(error);
-                  if (response.rows.length) {
-                    const userId = response.rows[0].user_id;
-                    resolve({ user_id });
-                  }
-                }
-            )
-        })
-      }
+static storeMappedItemsSetting({ user_id }) {
+  return new Promise((resolve, reject) => {
 
-      static getListItemMapping({ user_id }) {
-        return new Promise((resolve, reject) => {
-            pool.query(
-                `SELECT  mapped_items_to_categories FROM setting WHERE user_id=$1`,
-                [user_id],
-                (error, response) => {
-                  if (error) return reject(error);
-                  if (response.rows.length) {
-                    const mapped_items_to_categories = response.rows[0].mapped_items_to_categories;
-                    resolve({ mapped_items_to_categories });
-                  }
-                }
-            )
-        })
+    const client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+      statement_timeout: 10000,
+      query_timeout: 10000,
+      connectionTimeoutMillis: 10000
+    })      
+    client.connect()
+    client.query( `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
+      [user_id],
+      (error, response) => {
+        if (error) reject(error);
+        if (response.rows.length) {
+          const userId = response.rows[0].user_id;
+          resolve({ user_id });
+        }
+        client.end();
       }
+    )
 
-      static updateListItemMapping({ user_id }) {
-        return new Promise((resolve, reject) => {
-            pool.query(
-                `UPDATE setting SET mapped_items_to_categories=true WHERE user_id=$1`,
-                [user_id],
-                (error, response) => {
-                  if (error) return reject(error);
-                  resolve({ message: 'setting was updated' })
-                  // if (response.rows.length) {
-                  //   const mapped_items_to_categories = response.rows[0].mapped_items_to_categories;
-                  //   resolve({ mapped_items_to_categories });
-                  // }
-                }
-            )
-        })
-      }
+      //old pg
+      // pool.query(
+      //       `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
+      //       [user_id],
+      //       (error, response) => {
+      //         if (error) return reject(error);
+      //         if (response.rows.length) {
+      //           const userId = response.rows[0].user_id;
+      //           resolve({ user_id });
+      //         }
+      //       }
+      //   )
+    })
+  }
+
+  static getListItemMapping({ user_id }) {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            `SELECT  mapped_items_to_categories FROM setting WHERE user_id=$1`,
+            [user_id],
+            (error, response) => {
+              if (error) return reject(error);
+              if (response.rows.length) {
+                const mapped_items_to_categories = response.rows[0].mapped_items_to_categories;
+                resolve({ mapped_items_to_categories });
+              }
+            }
+        )
+    })
+  }
+
+  static updateListItemMapping({ user_id }) {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            `UPDATE setting SET mapped_items_to_categories=true WHERE user_id=$1`,
+            [user_id],
+            (error, response) => {
+              if (error) return reject(error);
+              resolve({ message: 'setting was updated' })
+              // if (response.rows.length) {
+              //   const mapped_items_to_categories = response.rows[0].mapped_items_to_categories;
+              //   resolve({ mapped_items_to_categories });
+              // }
+            }
+        )
+    })
+  }
 }
 
 module.exports = SettingTable;
