3aeb39cb6689e391077ab3167c5edaedce711b79
Carol Chung <cch5ng@gmail.com>
2020-08-11 14:06:15 -0700

    modify Pool config to include password

--- server/databasePool.js
+++ server/databasePool.js
@@ -1,19 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config()
 
-//const connectionString = `postgresql://${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
-
 const pool = new Pool({
 	user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
-  //password: process.env.PGPASSWORD,
+  password: process.env.PGPASSWORD,
 	port: process.env.PGPORT,
 	max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-})
-//const pool = new Pool();
+});
 
 //if (process.env.NODE_ENV === 'production') {
 //	connectionString = process.env.DATABASE_URL;
@@ -22,8 +19,6 @@ const pool = new Pool({
 	//databaseConfiguration = require('./secrets/databaseConfiguration');
 //	pool = new Pool(); //databaseConfiguration
 //}
-
-//module.exports = pool;
 
 module.exports = {
   query: (text, params, callback) => {
