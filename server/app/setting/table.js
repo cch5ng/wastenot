cfb6f8a24f4ef8b23c85b4cf43bffd1fe067a904
Carol Chung <cch5ng@gmail.com>
2020-08-11 13:59:17 -0700

    cleanup and revert to use pool from databasePool.js

--- server/app/setting/table.js
+++ server/app/setting/table.js
@@ -1,52 +1,25 @@
-const {Client} = require('pg');
-const pool = require('../../databasePool');
+const db = require('../../databasePool');
 
 class SettingTable {
-
-static storeMappedItemsSetting({ user_id }) {
-  return new Promise((resolve, reject) => {
-
-    const client = new Client({
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      password: process.env.PGPASSWORD,
-      port: process.env.PGPORT,
-      statement_timeout: 10000,
-      query_timeout: 10000,
-      connectionTimeoutMillis: 10000
-    })      
-    client.connect()
-    client.query( `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
-      [user_id],
-      (error, response) => {
-        if (error) reject(error);
-        if (response.rows.length) {
-          const userId = response.rows[0].user_id;
-          resolve({ user_id });
-        }
-        client.end();
-      }
-    )
-
-      //old pg
-      // pool.query(
-      //       `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
-      //       [user_id],
-      //       (error, response) => {
-      //         if (error) return reject(error);
-      //         if (response.rows.length) {
-      //           const userId = response.rows[0].user_id;
-      //           resolve({ user_id });
-      //         }
-      //       }
-      //   )
+  static storeMappedItemsSetting({ user_id }) {
+    return new Promise((resolve, reject) => {
+      db.query(
+            `INSERT INTO setting (user_id) VALUES ($1) RETURNING user_id`,
+            [user_id],
+            (error, response) => {
+              if (error) return reject(error);
+              if (response.rows.length) {
+                const userId = response.rows[0].user_id;
+                resolve({ user_id });
+              }
+            }
+        )
     })
   }
 
   static getListItemMapping({ user_id }) {
     return new Promise((resolve, reject) => {
-        pool.query(
+        db.query(
             `SELECT  mapped_items_to_categories FROM setting WHERE user_id=$1`,
             [user_id],
             (error, response) => {
@@ -62,7 +35,7 @@ static storeMappedItemsSetting({ user_id }) {
 
   static updateListItemMapping({ user_id }) {
     return new Promise((resolve, reject) => {
-        pool.query(
+        db.query(
             `UPDATE setting SET mapped_items_to_categories=true WHERE user_id=$1`,
             [user_id],
             (error, response) => {
