b49da69f9b478575dedebfae047bdc76b908b460
Carol Chung <cch5ng@gmail.com>
2020-08-29 16:19:19 -0700

    modify layout of alerts; add ability to filter alerts by days forward

--- client/src/Alerts/Alerts.js
+++ client/src/Alerts/Alerts.js
@@ -9,10 +9,12 @@ import React, { useState, useEffect } from 'react';
 import {connect} from 'react-redux';
 import http_requests from '../utils/http_requests';
 import Button from '../App/Shared/Button/Button';
+import './style.css';
 
 const Alerts = (props) => {
     const [alertsAr, setAlertsAr] = useState([]);
     const [alertsObj, setAlertsObj] = useState({});
+    const [daysUntilExpiration, setDaysUntilExpiration] = useState(7);
 
     function handlePostponeAlert(ev) {
         ev.preventDefault();
@@ -60,6 +62,11 @@ const Alerts = (props) => {
         }
     }
 
+    function handleRangeChange(ev) {
+        let daysCount = ev.target.value;
+        setDaysUntilExpiration(daysCount);
+    }
+
     function getAlertsObj(ar) {
         let alertsObj = {};
         ar.forEach(alert => {
@@ -67,6 +74,28 @@ const Alerts = (props) => {
             alertsObj[guid] = alert;
         })
         return alertsObj;
+    }
+
+    function getFilteredAlerts() {
+        let n = Date.now();
+        let additionalMs = convertDayToMs(daysUntilExpiration);
+        let thresholdMs = n + additionalMs;
+
+        let filteredAlerts = alertsAr.filter(alert => {
+            let expiryMs = convertISOStrToMs(alert.notify_timestamp);
+            return expiryMs <= thresholdMs;
+        })
+        return filteredAlerts;
+
+    }
+
+    function convertISOStrToMs(isoStr) {
+        let d = new Date(isoStr);
+        return d.getTime();
+    }
+
+    function convertDayToMs(dayCnt) {
+        return dayCnt * 24 * 60 * 60 * 1000;
     }
 
     //get init list of alerts to display
@@ -89,18 +118,27 @@ const Alerts = (props) => {
         setAlertsAr(newAlertsAr);    
     }, [alertsObj])
 
+    let filteredAlerts = daysUntilExpiration === 7 ? alertsAr : getFilteredAlerts();
+    console.log('alerts count', filteredAlerts.length)
     return (
-        <div>
+        <div className="main">
             <h1>Alerts</h1>
-            {alertsAr.map(alert =>
-                (<div>
+            <div>
+                <h2>Filter Alerts</h2>
+                <div className="form_group">
+                    <input type="range" id="days_until_expiration" name="days_until_expiration"
+                        onChange={handleRangeChange}
+                        min="1" max="7" className="input_range" />
+                    <label htmlFor="days_until_expiration">Days until expiration ({daysUntilExpiration})</label>
+                </div>
+            </div>
+            {filteredAlerts.map(alert =>
+                (<div className="row">
                     <p>{alert.name} will expire on {alert.notify_timestamp}</p>
-                    <Button onClickHandler={handlePostponeAlert} label="Postpone (1 day)" classVal={alert.guid}
-                        size="medium" />
-                    <Button onClickHandler={handleCancelAlert} label="Cancel alert" classVal={alert.guid}/>
+                    <Button onClickHandler={handlePostponeAlert} label="+1 day" classVal={alert.guid}/>
+                    <Button onClickHandler={handleCancelAlert} label="Cancel" classVal={alert.guid}/>
                 </div>)
             )}
-
         </div>
     )
 }
