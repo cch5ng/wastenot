4b7b19ffc2d0417ddd2612a787ad044feb0a65ba
Carol Chung <cch5ng@gmail.com>
2020-09-01 11:58:34 -0700

    convert internal notification to typescript file

--- client/src/App/InternalNotification/InternalNotification.tsx
+++ client/src/App/InternalNotification/InternalNotification.tsx
@@ -5,13 +5,16 @@ import { Link } from 'react-router-dom';
 import './style.css';
 import Checkbox from '../Shared/Checkbox/Checkbox';
 
+type InternalNotificationProps = {
+  clickCloseHandler: any
+}
 const InternalNotification = (props) => {
   return (
     <div className="internalNotification">
       <div className="controlClose" onClick={props.clickCloseHandler}>X</div>
       <p>This is a reminder to <Link to="/settings/expirationMapTest" onClick={props.clickCloseHandler}>set up mapping between your shopping list items and expiration dates.</Link></p>
       <form>
-        <Checkbox checkboxVal={false} checkboxLabel="Hide this message in the future" onChangeHandler={props.checkboxSelectHandler} />
+        <Checkbox checkboxVal={false} checkboxLabel="Hide this message in the future" onChangeHandler={props.checkboxSelectHandler} id='' name='' checkClassName={null} />
       </form>
     </div>
   )
