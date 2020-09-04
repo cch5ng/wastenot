5caff2d5a2f2e227d1b92d6c3563f54c50188c86
Carol Chung <cch5ng@gmail.com>
2020-09-03 18:23:44 -0700

    convert shopping list form no expiration to typescript

--- client/src/ShoppingLists/ShoppingListFormNoExpiration.tsx
+++ client/src/ShoppingLists/ShoppingListFormNoExpiration.tsx
@@ -10,23 +10,38 @@ import { isUsingExpiration } from '../actions/setting';
 import { objToArray, getCookieStr, arrayToObj } from '../utils/utils';
 import { render } from 'react-dom';
 
+
+type ShoppingListFormNoExpirationProps = {
+    title: string, 
+    formSubmitHandler: any, 
+    listName: string, 
+    inputChangeHandler: any, 
+    onClickHandler: any, 
+    renderForm: any
+}
 const ShoppingListFormNoExpiration = (props) => {
     const { title, formSubmitHandler, listName, inputChangeHandler, onClickHandler, renderForm } = props;
     return (
         <div>
             <h3>{title}</h3>
             <div>
-            <Button classVal="listDetailFormSaveBtn" onClickHandler={formSubmitHandler} label="Save" />
-            <Button label="Cancel" onClickHandler={onClickHandler} />
+            <Button classVal="listDetailFormSaveBtn" onClickHandler={formSubmitHandler} label="Save" 
+                idVal='' size={null} type={null} noMargin={false} />
+            <Button label="Cancel" onClickHandler={onClickHandler} 
+                classVal='' idVal='' size={null} type={null} noMargin={false} />
             </div>
             <br />
-            <InputText name="listNameInp" value={listName} placeholder="list name" onChangeHandler={inputChangeHandler} />
+            <InputText name="listNameInp" value={listName} placeholder="list name" 
+                onChangeHandler={inputChangeHandler} id='' inputClassName='' 
+                readOnly={false} type={null} />
             <ul className="list-no-style">
             {renderForm()}
             </ul>
             <div>
-            <Button classVal="listDetailFormSaveBtn" onClickHandler={formSubmitHandler} label="Save" />
-            <Button label="Cancel" onClickHandler={onClickHandler} />
+            <Button classVal="listDetailFormSaveBtn" onClickHandler={formSubmitHandler} label="Save"
+                idVal='' size={null} type={null} noMargin={false} />
+            <Button label="Cancel" onClickHandler={onClickHandler}
+                classVal='' idVal='' size={null} type={null} noMargin={false} />
             </div>    
         </div>
     )
