6870df683f001839cc7a503313e2dd95454a0801
Carol Chung <cch5ng@gmail.com>
2020-10-14 22:23:51 -0700

    add config for passport and passport-github2

--- server/app/index.js
+++ server/app/index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const express = require('express');
+const session = require("express-session");
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -16,6 +19,8 @@ const listItemMapRouter = require('./api/list_item_map');
 //   	throw result.error
 // 	}
 // }
+
+
 
 const app = express();
 let allowed;
@@ -34,6 +39,50 @@ app.use(function(req, res, next) {
 //app.use(cors({ origin: allowed, credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(session({ secret: process.env.EXPRESS_SECRET }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the GitHubStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   profile), and invoke a callback with a user object.
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+app.use(passport.session());
 
 app.use('/api/list', listRouter);
 app.use('/api/auth', authRouter);
@@ -55,5 +104,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname+'/../dist/index.html'));
   });
 }
+
+
 
 module.exports = app;
