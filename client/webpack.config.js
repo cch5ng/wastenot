e6bd3a328603a0d8873fccb26969a60e4617542a
Carol Chung <cch5ng@gmail.com>
2020-11-05 11:35:10 -0800

    cleanup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {InjectManifest} = require('workbox-webpack-plugin');
+const loader = require('sass-loader');
 //const Autoprefixer = require('autoprefixer');
 
 const devMode = process.env.NODE_ENV !== 'production'
@@ -31,27 +32,14 @@ module.exports = () => {
           use: ['babel-loader']
         },
         { test: /\.tsx?$/, loader: 'ts-loader' },
-        // {
-        //   test: /\.css$/i,
-        //   exclude: /node_modules/,
-        //   use: [
-        //     'style-loader',
-        //     {
-        //       loader: 'css-loader',
-        //       options: {
-        //         modules: true,
-        //       },
-        //     },
-        //   ],
-        // },
         {
           test: /\.(sa|sc|c)ss$/,
-          use: ['style-loader', 
-            'css-modules-typescript-loader',
-          //MiniCssExtractPlugin.loader,
+          use: ['style-loader',
+            //'typescript-plugin-css-modules',
+            //MiniCssExtractPlugin.loader,
             'css-loader',
             //'postcss-loader',
-            'sass-loader',
+            //'sass-loader',
           ]
         }
       ],
@@ -66,13 +54,13 @@ module.exports = () => {
         title: 'Waste Not',
         template: 'src/index.html'
       }),
-      new MiniCssExtractPlugin({
+      //new MiniCssExtractPlugin({
         // Options similar to the same options in webpackOptions.output
         // both options are optional
-        filename: 'style.[contenthash].css'
+        //filename: 'style.[contenthash].css'
         //filename: "[name].css",
         //chunkFilename: "[id].css"
-      }),
+      //}),
       new InjectManifest({
         swSrc: './src/sw.js',
       }),
