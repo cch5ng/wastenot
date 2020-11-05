9368e67de4323ffdd929e8990d37c74363e66979
Carol Chung <cch5ng@gmail.com>
2020-11-05 11:41:49 -0800

    typescript file for styles

---
+++ client/styles.d.ts
@@ -0,0 +1,16 @@
+// For CSS
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+// For SCSS
+// declare module "*.module.scss" {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
