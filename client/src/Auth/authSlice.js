cd638e6ee3f59fd9ec2d83541a903a0b412aa539
Carol Chung <cch5ng@gmail.com>
2021-06-18 21:33:54 -0700

    refactor auth reducer to use createSlice, createAsyncThunk from redux/toolkit

---
+++ client/src/Auth/authSlice.js
@@ -0,0 +1,120 @@
+import {
+  createSlice,
+  nanoid,
+  createAsyncThunk,
+  createEntityAdapter,
+} from '@reduxjs/toolkit'
+import * as serviceWorker from '../sw';
+
+import http_requests from '../utils/http_requests';
+import { getCookieStr } from '../utils/utils';
+
+const initialState = {
+  isLoggedIn: false,
+  hasButtonClicked: false,
+}
+
+export const register = createAsyncThunk('auth/register', async ({ email, password }) => {
+  const response = await http_requests.Auth.postRegister(email, password)
+  let cookieAr;
+
+  if (response.cookie && storageAvailable('sessionStorage')) {
+    cookieAr = response.cookie.split('=');
+    sessionStorage.setItem(cookieAr[0], cookieAr[1]);
+  }
+
+  return {
+    message: response.message,
+    authStr: cookieAr[1]
+  }
+})
+
+export const login = createAsyncThunk('auth/login', async ({ email, password }) => {
+  const response = await http_requests.Auth.postLogin(email, password)
+  let cookieAr;
+
+  if (response.cookie && storageAvailable('sessionStorage')) {
+    cookieAr = resp.cookie.split('=');
+    sessionStorage.setItem(cookieAr[0], cookieAr[1]);
+  }
+  serviceWorker.subscribeUserToPush();
+
+  return {
+    message: response.message,
+    authStr: cookieAr[1]
+  }
+})
+
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+  },
+  extraReducers: {
+    [register.pending]: (state, action) => {
+      state.status = 'loading'
+      state.error = null
+    },
+    [register.fulfilled]: (state, action) => {
+      if (state.status === 'loading') {
+        state.message = action.payload.message;
+        state.authStr = action.payload.authStr;
+        state.status = 'succeeded'
+      }
+    },
+    [register.rejected]: (state, action) => {
+      if (state.status === 'loading') {
+        state.status = 'failed'
+        state.error = action.payload
+      }
+    },
+    [login.pending]: (state, action) => {
+      state.status = 'loading'
+      state.error = null
+    },
+    [login.fulfilled]: (state, action) => {
+      if (state.status === 'loading') {
+        state.message = action.payload.message;
+        state.authStr = action.payload.authStr;
+        state.status = 'succeeded'
+      }
+    },
+    [login.rejected]: (state, action) => {
+      if (state.status === 'loading') {
+        state.status = 'failed'
+        state.error = action.payload
+      }
+    },
+  },
+})
+
+//helper
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+
+function storageAvailable(type) {
+  var storage;
+  try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+  }
+}
+
+export default authSlice.reducers;
