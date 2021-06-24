50e109a48567c26fdaca0e9d69bcb3c2452731af
Carol Chung <cch5ng@gmail.com>
2021-06-24 15:23:41 -0700

    refactor pointer to auth reducer

--- client/src/App/store.js
+++ client/src/App/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from '../Auth/authSlice';
+import authReducer  from '../Auth/authSlice';
 
 export default configureStore({
   reducer: {
