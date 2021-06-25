18dda8fc6316737c5e5cbb93a1a205469437e55d
Carol Chung <cch5ng@gmail.com>
2021-06-25 14:14:20 -0700

    add setting reducer to store

--- client/src/App/store.js
+++ client/src/App/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer  from '../Auth/authSlice';
+import settingReducer from '../Settings/settingSlice';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    setting: settingReducer,
   },
 })
