205b3fc89b6dd88fa56f317476dfc9b108479db2
Carol Chung <cch5ng@gmail.com>
2021-07-05 21:41:02 -0700

    fix missing type related to using useSelector hook

--- client/src/App/store.ts
+++ client/src/App/store.ts
@@ -10,3 +10,8 @@ export default configureStore({
     shoppingLists: shoppingListsReducer,
   },
 })
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
